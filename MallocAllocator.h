92962ccc0c9b0058ec0aa9d59594f47d3bed4a85
Ayxan <45293675+Ayxan13@users.noreply.github.com>
2020-03-08 11:02:03 +0100

    Fixed const qualifier bug

--- MallocAllocator.h
+++ MallocAllocator.h
@@ -4,13 +4,12 @@
  *
  *
  * Simple allocator that uses malloc as it's memory source
- * 
+ *
  */
 
 #ifndef MALLOC_ALLOCATOR_H
 #define MALLOC_ALLOCATOR_H
 #pragma once // Won't hurt https://stackoverflow.com/a/1144110/10147399
-
 
 #include <cstdlib>   // malloc/free
 #include <stdexcept> // bad_alloc
@@ -18,42 +17,42 @@
 template<class T>
 struct MallocAllocator
 {
-        using size_type = std::size_t;
-        using difference_type = std::ptrdiff_t;
-        using value_type = T;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+  using value_type = T;
 
-        MallocAllocator() noexcept = default;
-        template<class U>
-        MallocAllocator(const MallocAllocator<U>&) noexcept
-        {}
+  MallocAllocator() noexcept = default;
+  template<class U>
+  MallocAllocator(const MallocAllocator<U>&) noexcept
+  {}
 
-        T* allocate(size_type const s, void const* = nullptr) const
-        {
-                if (s == 0)
-                        return nullptr;
+  T* allocate(size_type const s, void const* = nullptr) const
+  {
+    if (s == 0)
+      return nullptr;
 
-                T* temp = static_cast<T*>(std::malloc(s * sizeof(T)));
+    T* temp = static_cast<T*>(std::malloc(s * sizeof(T)));
 
-                if (!temp)
-                        throw std::bad_alloc();
+    if (!temp)
+      throw std::bad_alloc();
 
-                return temp;
-        }
-        void deallocate(T* p, size_type) const noexcept { std::free(p); }
+    return temp;
+  }
+  void deallocate(T* p, size_type) const noexcept { std::free(p); }
 
-        template<class U>
-        bool operator==( MallocAllocator<U>& const)
-        {
-                return true;
-        }
+  template<class U>
+  bool operator==(MallocAllocator<U> const&)
+  {
+    return true;
+  }
 
-        template<class U>
-        bool operator!=(MallocAllocator<U>& const)
-        {
-                return false;
-        }
+  template<class U>
+  bool operator!=(MallocAllocator<U> const&)
+  {
+    return false;
+  }
 
-        using propagate_on_container_move_assignment = std::true_type;
+  using propagate_on_container_move_assignment = std::true_type;
 };
 
 #endif // !MALLOC_ALLOCATOR_H
